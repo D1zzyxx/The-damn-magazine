ce8b2d3ea4f4b5d2b62d2b2b6c3c6c753c9553f3
Error404Natures <166064123+Error404Natures@users.noreply.github.com>
2025-02-07 20:05:54 +0500

    Works?
    Works????

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    //ֿטחהא סכמזםûי סךנטןע אאמאמאמאמאמא
+    private float gravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
