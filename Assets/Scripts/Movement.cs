09a13fcf20a008cc6c751bf1d4499f1e88a0de95
Error404Natures <166064123+Error404Natures@users.noreply.github.com>
2025-02-07 23:17:40 +0500

    PerfomanceTest
    Изменил скрпит movement на отслеживание кликов и отображение их количества
    Добавил UI и TMPUGUI

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private int check = 0;
+    public TextMeshProUGUI TMPUGUI;
 
-    //Пизда сложный скрипт ааоаоаоаоаоа
-    private float gravity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        gravity = 1;
-    }
-
-    // Update is called once per frame
+    //Данный код создан ИСКЛЮЧИТЕЛЬНО В КАЧЕСТВЕ ПРОВЕРКИ!
+    //Он не несет в себе никакой смысловой нагрузки
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            check++;
+            Debug.Log("Очень сложный скрипт на проверку изменений в репозитории");
+            Debug.Log($"Количество кликов: {check}");
+
+            UpdateUI();
+        }
+    }
+
+    void UpdateUI()
+    {
+        if (TMPUGUI != null)
+        {
+            TMPUGUI.text = "Кликов: " + check.ToString();
+        }
     }
 }
