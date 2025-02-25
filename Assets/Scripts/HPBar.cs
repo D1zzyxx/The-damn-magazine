add40c518033d3d8467d87433856dca5c7b02e01
Arinabbbshka <arinabakieva58@gmail.com>
2025-02-25 22:06:22 +0500

    Система жизнейи их сохранение
    При взаимодействии с тегом "Door", происходит запись данных о количестве жизней персонажа

--- Assets/Scripts/HPBar.cs
+++ Assets/Scripts/HPBar.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
-    private float HP = 5f;
-    public Image Bar;   
+    private float HP;
+    public Image Bar;
     //HP --1;
     //private Bar.fillAmount = HP/5;
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("Hearts")) HP = 5f; //Проверяет наличие ключа "Hearts". При его отсутствии, HP будет равен 5
+        else PlayerPrefs.GetFloat("Hearts", HP);  // Иначе HP равен данным, которые были сохранены ранее
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D colDoor) //При взаимодействии с тегом Door, происходит запись количества жизней
+    {
+        if (colDoor.gameObject.tag == "Door")
+        {
+            CloseScene();
+        }
+    }
+    void CloseScene()
+    {
+        PlayerPrefs.SetFloat("Hearts", HP); //Сохранение в ключе "Hearts" данных о количестве жизней
+    }
 }
+ 
