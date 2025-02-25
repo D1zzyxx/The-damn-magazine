ee21b416665b846d5e6fcc65c3a251d46978939e
Arinabbbshka <arinabakieva58@gmail.com>
2025-02-25 21:08:15 +0500

    Создала шкалу жизни
    Отсутствуют условия, из-за которых уменьшается количество жизни

---
+++ Assets/Scripts/HPBar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    private float HP = 5f;
+    public Image Bar;   
+    //HP --1;
+    //private Bar.fillAmount = HP/5;
+}
