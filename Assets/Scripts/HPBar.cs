f8d4f9538873a51e744b6314bd85f87b8cda0107
Arinabbbshka <arinabakieva58@gmail.com>
2025-03-01 20:27:25 +0500

    Создала уменьшение жизней в ХПбаре

--- Assets/Scripts/HPBar.cs
+++ Assets/Scripts/HPBar.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
     private float HP;
     public Image Bar;
-    //HP --1;
-    //private Bar.fillAmount = HP/5;
+
     private void Awake()
     {
         if (!PlayerPrefs.HasKey("Hearts")) HP = 5f; //Проверяет наличие ключа "Hearts". При его отсутствии, HP будет равен 5
@@ -18,18 +19,30 @@ public class HPBar : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        
+        if (HP ==0) //При нулевом хп переносит на новую сцену
+        {
+            YouLose();
+        }
     }
-    void OnCollisionEnter2D(Collision2D colDoor) //При взаимодействии с тегом Door, происходит запись количества жизней
+    void OnCollisionEnter2D(Collision2D collision) //При взаимодействии с тегом Door, происходит запись количества жизней
     {
-        if (colDoor.gameObject.tag == "Door")
+        if (collision.gameObject.tag == "Door") //При взаимодействии с тегом Door, происходит запись количества жизней
         {
             CloseScene();
+        }
+        if (collision.gameObject.tag == "Block") //При взаимодействии с тегом Block, уменьшается количество жизней
+        {
+            HP -= 1;
+            Bar.fillAmount = HP / 5;
         }
     }
     void CloseScene()
     {
         PlayerPrefs.SetFloat("Hearts", HP); //Сохранение в ключе "Hearts" данных о количестве жизней
+    }
+    public void YouLose() //Переход на другую сцену
+    {
+        SceneManager.LoadScene(2);
     }
 }
  
