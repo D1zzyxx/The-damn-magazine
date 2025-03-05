8c28b27aefa0f1456f3ec4e100808cf7b1058cbe
Error404Natures <166064123+Error404Natures@users.noreply.github.com>
2025-03-05 12:14:40 +0500

    Squashed commit of the following:
    commit 19ded52e162d226926c97224d93bef95aae4a5ca
    Merge: 63206cc fe73e4d
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Wed Mar 5 11:41:43 2025 +0500
    
        Merge branch 'branchIlgizar'
    
    commit 63206ccc6af9666602c91f8db327d7eef315f53d
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Mon Feb 24 19:06:51 2025 +0500
    
        Передвижение камеры за персонажем
    
        Небольшие исправления в файлах: одна папка префабов, удален клон, отвечающий за систему передвижения персонажа
        Добавлено начальное следование камеры за персонажем, необходима дороботка
    
    commit df5cbece697f530c3a80b7296b1a26f5e1de096a
    Author: Roman5522 <r.radygin@yandex.ru>
    Date:   Sun Feb 23 16:21:30 2025 +0500
    
        Physics Classroom (физика)
    
    commit 4fd1e8e25712d597023ed0e46d3f54c79684729a
    Author: Roman5522 <r.radygin@yandex.ru>
    Date:   Sun Feb 23 15:35:34 2025 +0500
    
        Кабинет английского
    
    commit 409f3889837b08e173bd97f1459af6011ef94628
    Merge: d25de86 2dcad03
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Sun Feb 23 10:01:17 2025 +0500
    
        Merge branch 'branchYara'
    
    commit 2dcad03cb248d0612df63d972ebc200bde1e4619
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Sun Feb 23 09:55:17 2025 +0500
    
        Использование предметов
    
        Добавлена функция использования предметов из инвентаря
    
    commit 8208eb702b9b7a9de22ce9f663eb8c6ec4d5594a
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Thu Feb 20 20:11:28 2025 +0500
    
        Добавление информации об объекте
    
        Окно информации слота, содержащие иконку, наименование, описание, лорное описание, кнопку использования(нерабочая), кнопку выкидывания.
    
    commit ab155aa8fa247ab293b8c09959e181600764ea50
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Wed Feb 19 13:12:11 2025 +0500
    
        Добавление инвентаря
    
        Добавлено отображение инвентаря, написаны скрипты для инвентаря, слотов, предметов, поднятие предметов.
        На игрока добавлен тег "Player" и квадратный коллайдер
    
    commit 0dc23996cfead78f3a06451303041758240795b7
    Author: Dizzyxx <yrvark800@gmail.com>
    Date:   Wed Feb 19 11:37:37 2025 +0500
    
        Squashed commit of the following:
    
        commit 500d6bf7f2b43aeb6a3091c9477c9d778690f1c4
        Author: Dizzyxx <yrvark800@gmail.com>
        Date:   Wed Feb 19 11:05:02 2025 +0500
    
            Ходьба, полностью
    
            Исправлена ошибка конфликта файлов, которая мешала работе передвижения
    
        commit 973401870b6cf71d7c4cd7791cb25685d955895f
        Merge: d53a33d 305c8a3
        Author: Dizzyxx <yrvark800@gmail.com>
        Date:   Wed Feb 19 10:49:20 2025 +0500
    
            Merge branch 'branchArina'
    
        commit 305c8a3d2094a5af9075bbfbeb0efe50ca97b45c
        Author: Dizzyxx <yrvark800@gmail.com>
        Date:   Wed Feb 19 10:38:26 2025 +0500
    
            Починка ветоок
    
        commit 56fef0bbafa534793c2c5350703ba408582d8844
        Author: Arinabbbshka <arinabakieva58@gmail.com>
        Date:   Sun Feb 16 23:59:52 2025 +0500
    
            Создана система передвижения персонажа
    
            Персонаж теперь может передвигаться по локации
    
        commit d53a33da8af4d29216000748f845a65745dee26e
        Author: Arinabbbshka <arinabakieva58@gmail.com>
        Date:   Sat Feb 15 21:18:53 2025 +0500
    
            Создала персонажа
    
            Добавила System Input, без возможности передвижения персонажа

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance;
+
+    [SerializeField] private float movingSpeed = 5f;
+    private PlayerInputActions playerInputActions; //Переменная, которая подключает скрипт движения персонажа
+
+    private Rigidbody2D rb; //Создаю переменную для компонента Rigidbody, которая позволяет управлять персонажем
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this; //инициализируем статическое поле
+        }
+
+        rb = GetComponent<Rigidbody2D>(); //Инициализирую компонент Rigidbody2D, которая позволяет персонажу иметь физические свойства
+        playerInputActions = new PlayerInputActions(); //Инициализирую Input System, она позволяет персонажу передвигаться  
+        playerInputActions.Enable(); //Подключение системы ввода(Input System)
+    }
+    private Vector2 GetMovementVector() // Функция, позволяющая получить вектор движения из переменной playerInputActionss
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>(); 
+
+        return inputVector;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = GetMovementVector(); //Переменная для хранения направления движения персонажа
+
+        inputVector = inputVector.normalized; //Делает так, чтобы скорость персонажа при движении в разных направлениях была одинакова
+
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime)); 
+        //Метод, который принимает позицию персонажа, и двигает объект в зависимости от его
+        //(текущего местоположения + направление движения * (заданную нами скорость * настраиваемый интервал смены частоты кадров))
+    }
+}
